456b0a45f74ceefe00775f25bd485dcb35849101
camilacf <camilacf2112@gmail.com>
2019-07-31 01:20:41 -0300

    Fixes animation timing and creates new tab

--- simulator/urls.py
+++ simulator/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('main', views.main, name='main'),
     path('animacao', views.animacao, name='animacao'),
     path('transiente', views.transiente, name='transiente'),
+    path('kmin', views.kmin, name='kmin'),
     re_path(r'^simular/(?P<rho>[0-9][.][0-9])/(?P<disciplina>[\w-]+)/(?P<kmin>[0-9]+)/(?P<rodadas>[0-9]+)/(?P<seed_esperta>[0-1])/$', views.simular_background, name='simular'),
     re_path(r'^simular_kmin/(?P<rho>[0-9][.][0-9])/(?P<disciplina>[\w-]+)/(?P<rodadas>[0-9]+)/(?P<seed_esperta>[0-1])/$', views.simular_background2, name='simular_kmin'),
     re_path(r'^simulartoplot/(?P<rho>[0-9][.][0-9])/(?P<disciplina>[\w-]+)/(?P<kmin>[0-9]+)/(?P<rodadas>[0-9]+)/$', views.simular_toplot, name='simular_toplot'),
